70642aa26e1fc899850fd9f23ee4a1072a96375c
Avinash Dhillor <avinashdhillor24@gmail.com>
2018-12-10 08:08:42 +0530

    Authentication setup done

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,6 +9,9 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //DB Config
 const db = 'mongodb://localhost:27017/devconnector';
 
@@ -20,16 +25,18 @@ mongoose
     console.log('error', err);
   });
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require('./config/passport.js')(passport);
 
 //Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
