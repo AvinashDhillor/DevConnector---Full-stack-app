3a8f343235e61efad6fa80464f349e19834711ba
Avinash Dhillor <avinashdhillor24@gmail.com>
2018-12-12 18:15:32 +0530

    Footer login register and landing page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navbar from '../src/components/layout/Navbar';
+import Footer from '../src/components/layout/Footer';
+import Landing from '../src/components/layout/Landing';
+import Register from '../src/components/auth/Register';
+import Login from '../src/components/auth/Login';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Navbar />
-        <h1>My React App</h1>
-      </div>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <Route path='/' component={Landing} exact />
+          <div className='container'>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
